5c08db90b18cbbc941e1128abaeca57696c0707e
Tom Wartmann <tomw@ethz.ch>
2023-04-14 13:34:37 +0200

    connect /reconsider endpoint to backend

--- react-frontend/src/sections/@dashboard/app/CandidatesPlot.js
+++ react-frontend/src/sections/@dashboard/app/CandidatesPlot.js
@@ -91,8 +91,9 @@ export default function CandidatesPlot({
   // load example person from backend
   const [dummyPersons, setDummyPersons] = useState([]);
   const fetchDummyPersonsData = () => {
-    fetch("http://127.0.0.1:8000/person/random/6")
+    fetch("http://127.0.0.1:8000/reconsider")
       .then((response) => {
+        console.log(response);
         return response.json();
       })
       .then((data) => {
