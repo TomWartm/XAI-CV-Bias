52e206dc73e674d854df7d676058e043bfa3f6a0
VitoPagone <cristianpagone1@gmail.com>
2023-06-08 13:48:34 +0200

    Random forest/Name Surname modifications

--- backend-project/src/dummy_server/ml.py
+++ backend-project/src/dummy_server/ml.py
@@ -7,11 +7,14 @@ import shap
 from sklearn.metrics import accuracy_score
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
+from shap import TreeExplainer
+from faker import Faker
 
 SUBSET_FRAC = 0.8#0.6
 
 def load_df():
-    df = pd.read_csv('data/dataset.csv')
+    df = pd.read_csv('backend-project/data/dataset.csv')
     df = df[df['company'] == 'C']
     df = df.drop(['company'], axis=1)
 
@@ -22,24 +25,25 @@ def load_df():
 
     return df
 
-
 def prepare_dataset(df):
+    fake = Faker()
+
     bool_cols = ['ind-debateclub', 'ind-programming_exp', 'ind-international_exp',
                  'ind-entrepeneur_exp', 'ind-exact_study', 'decision']
     df[bool_cols] = df[bool_cols].astype(int)
 
-    scaler = MinMaxScaler()
-    df[['age', 'ind-university_grade']
-       ] = scaler.fit_transform(df[['age', 'ind-university_grade']])
-
     cat_cols = ['gender', 'nationality', 'sport', 'ind-degree']
     df = pd.get_dummies(df, columns=cat_cols)
 
-    return df
+    df['name'] = [fake.first_name_male() if gender == 1 else fake.first_name_female() for gender in df['gender_male']] # adjust this line according to the exact column name after dummies transformation
+    df['surname'] = [fake.last_name() for _ in range(len(df))]
 
+    # Reordering the DataFrame to have 'name' and 'surname' as the first two columns
+    return df
 
 original_df = load_df()
 ml_dataset = prepare_dataset(original_df.copy())
+
 
 
 def get_sensitive_attributes():
@@ -59,41 +63,51 @@ def get_sensitive_attributes():
 
     return sensitive_attributes
 
-
 def get_shap_values():
     ids = ml_dataset["Id"]
-    X = ml_dataset.drop(columns=["decision", "Id"])
+    names = ml_dataset["name"]
+    surnames = ml_dataset["surname"]
+    X = ml_dataset.drop(columns=["decision", "Id","name","surname"])
     y = ml_dataset["decision"]
 
-    model = LogisticRegression(max_iter=1000)
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X, y)
     y_pred = model.predict(X)
-    accuracy = accuracy_score(y, y_pred)
-    # print(f"Logistic Regression accuracy: {accuracy:.4f}")
-    explainer = shap.Explainer(model, X)
-    shap_values = explainer(X)
 
-    shap_df = pd.DataFrame(shap_values.values, columns=X.columns)
-    shap_df["Id"] = ids.values
-    shap_df['predicted_decision'] = y_pred
-    shap_df["actual_decision"] = y.values
+    explainer = TreeExplainer(model)
+    shap_values = explainer.shap_values(X)
+    shap_values = shap_values[1] - shap_values[0] 
+
+    shap_df = pd.DataFrame(shap_values, columns=X.columns)
+    shap_df["bias_score"] = shap_df.sum(axis=1)
+    X = X.reset_index(drop=True)
+
+    df_with_shap = pd.concat([X, shap_df], axis=1)
+    print(df_with_shap.columns)
+
+    df_with_shap["Id"] = ids.values
+    df_with_shap["name"] = names.values
+    df_with_shap["surname"] = surnames.values
+    df_with_shap['predicted_decision'] = y_pred
+    df_with_shap["actual_decision"] = y.values
+    print(df_with_shap.columns)
+
 
     multiindex_vals = [
         ["bias", "fair", "fair", "fair", "fair", "fair", "fair", "fair", "bias", "bias", "bias", "bias", "bias", "bias",
-            "fair", "fair", "fair", "fair", "fair", "fair", "fair", "fair", "fair", "fair", "fair", "meta", "meta", "meta"],
+            "fair", "fair", "fair", "fair", "fair", "fair", "fair", "fair", "fair", "fair", "fair", "meta", "meta", "meta", "meta", "meta", "meta"],
         ['age', 'university_grade', 'debateclub', 'programming_exp',
          'international_exp', 'entrepeneur_exp', 'languages',
          'exact_study', 'gender', 'gender', 'gender',
          'nationality', 'nationality', 'nationality',
          'sport', 'sport', 'sport', 'sport', 'sport',
          'sport', 'sport', 'sport', 'degree',
-         'degree', 'degree', 'meta', 'meta',
-         'meta'],
-        shap_df.columns.values]
+         'degree', 'degree', 'meta', 'meta', 'meta', 'meta', 'meta', 'meta'],
+        df_with_shap.columns.values]
     multiindex = pd.MultiIndex.from_arrays(multiindex_vals)
-    shap_df.columns = multiindex
+    df_with_shap.columns = multiindex
 
-    return shap_df
+    return df_with_shap
 
 
 def build_scatterplot_data(shap_df):
@@ -169,6 +183,8 @@ def build_reconsider(scatter_df: pd.DataFrame):
 
 def build_similarpeople(scatter_df: pd.DataFrame, shap_df: pd.DataFrame):
     ids = shap_df.loc[:, (slice(None), slice(None), "Id")]
+    names = shap_df.loc[:, (slice(None), slice(None), "name")]
+    surnames = shap_df.loc[:, (slice(None), slice(None), "surname")]
 
     # Generate a matrix of cosine similarity between fair properties for each pair of people
     X1 = shap_df.loc[:, ("fair", slice(None), slice(None))]
@@ -196,16 +212,34 @@ def build_similarpeople(scatter_df: pd.DataFrame, shap_df: pd.DataFrame):
     result = {}
 
     for index, neighbor in enumerate(closest):
-        result[ids.iloc[index].values[0]] = {"1": ids.iloc[neighbor].values[0]}
+        id_key = ids.iloc[index].values[0]
+        name_key = names.iloc[index].values[0]
+        surname_key = surnames.iloc[index].values[0]
+
+        id_value = ids.iloc[neighbor].values[0]
+        name_value = names.iloc[neighbor].values[0]
+        surname_value = surnames.iloc[neighbor].values[0]
+
+        result[(id_key, name_key, surname_key)] = {
+            "1": (id_value, name_value, surname_value)
+        }
 
     return result
 
 
-def ignore_person(id):
+
+def ignore_person(id, name, surname):
     global original_df
     global ml_dataset
-    original_df = original_df.drop(original_df[original_df["Id"] == id].index)
-    ml_dataset = ml_dataset.drop(ml_dataset[ml_dataset["Id"] == id].index)
+
+    original_df = original_df.drop(original_df[(original_df["Id"] == id) & 
+                                               (original_df["name"] == name) & 
+                                               (original_df["surname"] == surname)].index)
+
+    ml_dataset = ml_dataset.drop(ml_dataset[(ml_dataset["Id"] == id) & 
+                                            (ml_dataset["name"] == name) & 
+                                            (ml_dataset["surname"] == surname)].index)
+
 
 
 def train_ml_model():
@@ -216,3 +250,12 @@ def train_ml_model():
     similar_people = build_similarpeople(scatter_df, shap_df)
 
     return (scatter_df, similar_people, reconsider, totals)
+
+scatter_df, similar_people, reconsider, totals = train_ml_model()
+print("Scatter DF: \n", scatter_df)
+print("Similar People: \n", similar_people)
+print("Reconsider: \n", reconsider)
+print("Totals: \n", totals)
+
+
+
